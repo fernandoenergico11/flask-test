9c879e75e2a3a0f1ca469ac7c44ad9a07675647b
fernandoenergico11 <84112843+fernandoenergico11@users.noreply.github.com>
2023-12-20 01:07:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS  # Añadido para el soporte CORS
 import pymysql
 
 app = Flask(__name__)
+CORS(app)  # Añadido para el soporte CORS
 
 # ... (otras rutas)
 
@@ -9,41 +11,32 @@ app = Flask(__name__)
 def mostrar_numeros():
     cantidad_aleatorios = 2
 
-    # Configuración de la conexión a la base de datos usando with statement
     try:
-        with pymysql.connect(host='bgumgxsdvc4biuaqa7lz-mysql.services.clever-cloud.com', user='un7kcgf6ih5t59l7', passwd='RaJQ617Jy7Nc9gcXvE90', db='bgumgxsdvc4biuaqa7lz') as miConexion:
+        with pymysql.connect(host='localhost', user='root', passwd='', db='bolsagana') as miConexion:
             cur = miConexion.cursor()
 
-            # Obtener la cantidad total de registros en la tabla para los números con estado=1
             cur.execute("SELECT COUNT(*) FROM grupo WHERE estado=1")
             total_registros_estado = cur.fetchone()[0]
 
-            # Verificar si hay suficientes registros activos para obtener la cantidad deseada
             if total_registros_estado < cantidad_aleatorios:
-                return "No hay suficientes registros activos para obtener la cantidad deseada."
+                return jsonify({"error": "No hay suficientes registros activos para obtener la cantidad deseada."})
             else:
-                # Crear la consulta SQL con los números aleatorios
                 consulta_sql = f"SELECT code FROM grupo WHERE estado=1 ORDER BY RAND() LIMIT 2"
                 cur.execute(consulta_sql)
 
-                # Obtener los códigos elegidos
                 elegidos = [code[0] for code in cur.fetchall()]
 
                 if len(elegidos) == cantidad_aleatorios:
-                    # Insertar los números aleatorios en la tabla "compra_boletas"
                     for num_aleatorio in elegidos:
                         cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (num_aleatorio,))
-                        # Actualizar el campo "estado" en la tabla "grupo"
                         cur.execute("UPDATE grupo SET estado = 0 WHERE code = %s", (num_aleatorio,))
 
-                    # Confirmar la transacción y cerrar la conexión
                     miConexion.commit()
-
-                    # Renderizar la plantilla HTML con los números aleatorios
-                    return render_template('numeros.html', numeros=elegidos)
+ 
+                    # En lugar de renderizar la plantilla HTML, ahora devolvemos los números aleatorios como JSON
+                    return jsonify({"numeros": elegidos})
     except pymysql.Error as e:
-        # Manejo de excepciones para errores de base de datos
-        return f"Error de base de datos: {e}"
+        return jsonify({"error": f"Error de base de datos: {e}"})
 
 if __name__ == '__main__':
     app.run(debug=True)
