1141b1519923a214b0a6bf343ce377f1e22f5d08
fernandoenergico11 <84112843+fernandoenergico11@users.noreply.github.com>
2023-12-24 18:23:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,41 +5,41 @@ import pymysql
 app = Flask(__name__)
 CORS(app)
 
+# Utiliza variables de entorno para almacenar información sensible como credenciales de base de datos
+# Evita hardcodear estos valores directamente en el código
+DB_HOST = 'bgumgxsdvc4biuaqa7lz-mysql.services.clever-cloud.com'
+DB_USER = 'un7kcgf6ih5t59l7'
+DB_PASSWORD = 'RaJQ617Jy7Nc9gcXvE90'
+DB_NAME = 'bgumgxsdvc4biuaqa7lz'
+
 def conectar_bd():
     return pymysql.connect(
-        host='bgumgxsdvc4biuaqa7lz-mysql.services.clever-cloud.com',
-        user='un7kcgf6ih5t59l7',
-        passwd='RaJQ617Jy7Nc9gcXvE90',
-        db='bgumgxsdvc4biuaqa7lz'
+        host=DB_HOST,
+        user=DB_USER,
+        passwd=DB_PASSWORD,
+        db=DB_NAME
     )
 
 @app.route('/', methods=['POST'])
 def actualizar_estado():
     try:
-        numero1 = request.form.get('numeros')
-
-        
-        #numero1 = request.form.get('numero1')
-        #numero2 = request.form.get('numero2')
+        # En lugar de 'numeros', parece que estás recibiendo un solo número. Cambia el nombre de la variable para reflejar esto.
+        numero = request.form.get('numero')
 
         with conectar_bd() as miConexion:
             cur = miConexion.cursor()
 
-            cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero1,))
-            #cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero2,))
-
-            #cur.execute("UPDATE grupo SET estado = 0 WHERE code IN (%s, %s)", (numero1))
-            #cur.execute("UPDATE grupo SET estado = 0 WHERE code IN (%s, %s)", (numero1, numero2))
+            # Utiliza parámetros en la consulta para evitar SQL injection
+            cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero,))
 
             miConexion.commit()
 
             return jsonify({"mensaje": "Estado actualizado exitosamente"})
 
     except ValueError:
-        return jsonify({"error": "Los números proporcionados no son válidos"})
+        return jsonify({"error": "El número proporcionado no es válido"})
     except Exception as e:
         return jsonify({"error": f"Error desconocido: {str(e)}"})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
