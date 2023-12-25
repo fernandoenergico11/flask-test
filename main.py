d97a67e92c5937c268fc4181b9898c46ec44dd44
fernandoenergico11 <84112843+fernandoenergico11@users.noreply.github.com>
2023-12-24 21:06:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 CORS(app)
 
 # Utiliza variables de entorno para almacenar información sensible como credenciales de base de datos
-# Evita hardcodear estos valores directamente en el código
 DB_HOST = 'bgumgxsdvc4biuaqa7lz-mysql.services.clever-cloud.com'
 DB_USER = 'un7kcgf6ih5t59l7'
 DB_PASSWORD = 'RaJQ617Jy7Nc9gcXvE90'
@@ -24,24 +23,27 @@ def conectar_bd():
 def actualizar_estado():
     try:
         # Cambia la forma de obtener el número cuando se envían datos JSON
-        numeros = request.json.get('numero')  # Se espera una lista de números
+        data = request.get_json()
+        numeros = data.get('numero')  # Se espera una lista de números
+
+        if not numeros or not isinstance(numeros, list):
+            raise ValueError("Se esperaba una lista de números en el campo 'numero'")
 
         with conectar_bd() as miConexion:
             cur = miConexion.cursor()
 
             for numero in numeros:
-                cur.execute("UPDATE grupo SET estado = 0 WHERE code = %s", (numero,))
-
-            # cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero,))
+                #cur.execute("UPDATE grupo SET estado = 0 WHERE code = %s", (numero,))
+                 cur.execute("UPDATE grupo SET estado = 0 WHERE code = 003")
 
             miConexion.commit()
 
             return jsonify({"mensaje": "Estado actualizado exitosamente"})
 
-    except ValueError:
-        return jsonify({"error": "El número proporcionado no es válido"})
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 400
     except Exception as e:
-        return jsonify({"error": f"Error desconocido: {str(e)}"})
+        return jsonify({"error": f"Error desconocido: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
