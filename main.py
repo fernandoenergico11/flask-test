493dbb6aacfec407e59cd1f76d5bde908fb798d5
fernandoenergico11 <84112843+fernandoenergico11@users.noreply.github.com>
2023-12-25 00:27:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ def conectar_bd():
         host='bgumgxsdvc4biuaqa7lz-mysql.services.clever-cloud.com',
         user='un7kcgf6ih5t59l7',
         passwd='RaJQ617Jy7Nc9gcXvE90',
-        db='bgumgxsdvc4biuaqa7lz'
+        db='bgumgxsdvc4biuaqa7lz',
+        cursorclass=pymysql.cursors.DictCursor  # Utilizar cursor de tipo DictCursor para obtener resultados como diccionarios
     )
 
-@app.route('/', methods=['POST'])  # Cambiado a POST
+@app.route('/', methods=['POST'])
 def actualizar_estado():
     try:
-        numero1 = request.form.get('numero1')  # Obtener el valor de 'numero1' desde la solicitud
-
+        numero1 = request.form.get('numero1')
+        
         with conectar_bd() as miConexion:
-            cur = miConexion.cursor()
+            with miConexion.cursor() as cur:
+                # Insertar números en la tabla compra_boletas
+                cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero1,))
 
-            # Insertar números en la tabla compra_boletas
-            cur.execute("INSERT INTO compra_boletas (code) VALUES (%s)", (numero1,))
-
-            # Actualizar estado en la tabla grupo
-            cur.execute("UPDATE grupo SET estado = 0 WHERE code = %s", (numero1,))
+                # Actualizar estado en la tabla grupo
+                cur.execute("UPDATE grupo SET estado = 0 WHERE code = %s", (numero1,))
 
             miConexion.commit()
 
